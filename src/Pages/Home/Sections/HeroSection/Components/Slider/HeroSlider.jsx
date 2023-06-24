1194c2e43f2914e6d45d2cb9896184e90fab9ed0
sabbir1054 <mdsabbir1054@gmail.com>
2023-06-24 13:36:04 +0600

    hero slider done

--- src/Pages/Home/Sections/HeroSection/Components/Slider/HeroSlider.jsx
+++ src/Pages/Home/Sections/HeroSection/Components/Slider/HeroSlider.jsx
@@ -1,83 +1,55 @@
-import React from 'react';
+import React from "react";
+import { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation } from "swiper";
+
 import { Box } from "@mui/material";
-import { ArrowRightAlt } from "@mui/icons-material";
-import { Link } from "react-router-dom";
-import styles from "./HeroSlide.module.scss"
-// Import Swiper styles
-import 'swiper/css';
+
 const HeroSlider = () => {
-    return (
-      <>
-        <Swiper
-          navigation={true}
-          modules={[Navigation, Autoplay]}
-          className={`${styles.swiper} swiperC`}
-          autoplay={{
-            delay: 6000,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }}
-        >
-          <SwiperSlide className={styles["swiper-slide"]} position="relative">
-            <Box component="div" className={styles.headerSliderText}>
-              <h3>Daily Deals</h3>
-              <h1>
-                AirPods
-                <br /> Earphones
-              </h1>
-              <p>
-                <sup className={styles.firstSup}>Today:</sup>
-                <span>
-                  $247<sup className={styles.secondSup}>.99</sup>
-                </span>
-              </p>
-              <Link to={"/product/59851231"} className={styles.bannerButton}>
-                Click Here <ArrowRightAlt sx={{ ml: 1, fontSize: 22 }} />
-              </Link>
-            </Box>
-            <Box
-              component="img"
-              className={styles.photo1}
-              src="https://firebasestorage.googleapis.com/v0/b/shop-8b88e.appspot.com/o/b19ec7de-2a28-4eff-9e3b-564f9864058b.jpg?alt=media&token=49243794-c5d4-4da9-8a28-2bd8053460d2"
+  return (
+    <>
+      <Swiper
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        autoplay={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Box>
+            <img
+              style={{ width: "100%" }}
+              src="https://i.ibb.co/5MbWKSY/img3.jpg"
+              alt=""
+              srcset=""
             />
-          </SwiperSlide>
-          <SwiperSlide className={styles["swiper-slide"]} position="relative">
-            <Box component="div" className={styles.headerSliderText}>
-              <h3>Deals And Promotions</h3>
-              <h1>
-                Echo Dot
-                <br />
-                3rd Gen
-              </h1>
-              <p>
-                <sup
-                  className={styles.firstSup}
-                  style={{ textDecoration: "line-through", color: "#333" }}
-                >
-                  $49.99
-                </sup>
-                <span>
-                  $29
-                  <sup className={styles.secondSup} style={{ top: "-0.2em" }}>
-                    .99
-                  </sup>
-                </span>
-              </p>
-              <Link to={"product/67384206"} className={styles.bannerButton}>
-                Click Here <ArrowRightAlt sx={{ ml: 1, fontSize: 22 }} />
-              </Link>
-            </Box>
-            <Box
-              component="img"
-              className={styles.photo2}
-              src="https://firebasestorage.googleapis.com/v0/b/shop-8b88e.appspot.com/o/27a789d0-6270-4fbf-aa6c-dd9d062070cd.jpg?alt=media&token=3dae993d-4ce1-4958-b5cb-490f44dc5601"
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box>
+            <img
+              style={{ width: "100%" }}
+              src="https://i.ibb.co/5MbWKSY/img3.jpg"
+              alt=""
+              srcset=""
             />
-          </SwiperSlide>
-        </Swiper>
-      </>
-    );
+          </Box>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Box>
+            <img
+              style={{ width: "100%" }}
+              src="https://i.ibb.co/5MbWKSY/img3.jpg"
+              alt=""
+              srcset=""
+            />
+          </Box>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 };
 
 export default HeroSlider;
